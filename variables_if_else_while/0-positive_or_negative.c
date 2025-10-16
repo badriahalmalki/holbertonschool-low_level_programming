ec1b07d043bb86eeda52c87a4ff1096e2c4493f1
badriahalmalki <badryahbalmalki@gmail.com>
2025-10-16 15:36:54 +0000

    1

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -9,7 +9,7 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
         if (n > 0) 
-	  {printf("is positive\n", n);}
+	  {printf("is positive\n");}
 	if (n < 0)
 	  {printf("is negative\n");}
 	return (0);
