e0069933b1031c56d553ab2efd31c556393ddae0
badriahalmalki <badryahbalmalki@gmail.com>
2025-10-18 14:57:57 +0000

    1

---
+++ functions_nested_loops/7-main.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int r;
+
+    r = print_last_digit(98);
+    _putchar('\n');
+    r = print_last_digit(0);
+    _putchar('\n');
+    r = print_last_digit(-1024);
+    _putchar('\n');
+
+    return (0);
+}
