5d4be25f0216c8b20c7d35e0ee9559e07352cee3
badriahalmalki <badryahbalmalki@gmail.com>
2025-10-21 03:24:58 +0000

    1

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,12 +1,10 @@
 #include "main.h"
 
-
 /**
  * _isupper - checks for uppercase character
- * @c: character to check  
+ * @c:character to check
  * Return: 1 if c is uppercase, 0 otherwise
  */
-
 
 int _isupper(int c)
 
