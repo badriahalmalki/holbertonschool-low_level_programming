5b7ce7514db51106fa99e74302f7bbc67b443b51
badriahalmalki <badryahbalmalki@gmail.com>
2025-10-23 10:33:29 +0000

    1

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_diagonal - draws a diagonal line
+ * @n: print  _
+ * Return: void
+ */
+void print_diagonal(int n)
+{
+int i;
+if (n <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for ( = 0; i < n; i++)
+_putchar('_');
+_putchar('\n');
+}
+}
