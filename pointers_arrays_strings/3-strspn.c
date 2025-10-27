3bb3923b3f3c787f2768ce1734c8c5b07c23ad25
badriahalmalki <badryahbalmalki@gmail.com>
2025-10-27 15:04:05 +0000

    fix

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -1,22 +1,29 @@
 #include "main.h"
 #include <stddef.h>
 /**
- * _strspn - gets the length of a prefix substring
- * @s: the string we want to analyze
- * @accept: the string containing the characters we want to check against
- * return: the number of bytes in the initial segment of s that consist only of characters found in accept
- */
+* _strspn - gets the length of a prefix substring
+* @s: the string we want to analyze
+* @accept: the string containing the characters we want to check against
+* return: the number of bytes in the initial segment of s that consist only of characters found in accept
+*/
 unsigned int _strspn(char *s, char *accept)
 {
 unsigned int count = 0;
 int i, j; 
-for (i =0; s[i] != '\0'; i++)
+for (i = 0; s[i] != '\0'; i++)
 {
-if (s[i] == accept [j])
+for (j = 0; accept[j] != '\0'; j++)
+{
+if (s[i] == accept[j])
+{
 count++;
+break;
 }
+}
+if (accept[j] == '\0')
 {
-if (accept [j] == '\0')
+break;
+}
 }
 return count;
 }
