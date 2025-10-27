ec95e1bb3ecccf0458aa66459106159a59725dc9
badriahalmalki <badryahbalmalki@gmail.com>
2025-10-27 06:52:06 +0000

    satart

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
-* _memcpy - copies memory area
-* @n: the byet the we want to copy
-* @srs: the memory area
-* @dest: whare we want the copy to go
-* return: a pointer to dest
+*_memcpy - copies memory area
+*@n: the byet the we want to copy
+*@srs: the memory area
+*@dest: whare we want the copy to go
+*return: a pointer to dest
 */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
