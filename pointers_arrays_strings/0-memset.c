fccd7424c509efc54cf7b3bb04a0298ed522c8f5
badriahalmalki <badryahbalmalki@gmail.com>
2025-10-27 05:35:32 +0000

    satart

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ *_memset - fills the first n bytes of the memory area pointed to by s with the constant byte b
+ * @n
+ * @s
+ * @b
+ * retern: a pointer to the memory area s
+ */
+char *_memset(char *s, char b, unsigned int n);
+
