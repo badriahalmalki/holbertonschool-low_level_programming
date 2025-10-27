0a39f6953923e2ed4f64ce802e26dfc885c33371
badriahalmalki <badryahbalmalki@gmail.com>
2025-10-27 12:31:38 +0000

    fixing it

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stddef.h>
+/**
+ * _strchr - 
+ * @c: the character to locate
+ * @s: the string
+ * retern: a pointer or null
+ */
+char *_strchr(char *s, char c)
+{
+unsigned int i;
+while (*s != '\0')
+{
+if ((unsigned char)*s == s)
+{return (char *)s;
+S++;
+if (s == '\0')
+{return (cHAR *)S;
+}
+return NULL;
+}
