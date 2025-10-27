8663ef85f21319ba232cf63dd0ddac54f6f36b7f
badriahalmalki <badryahbalmalki@gmail.com>
2025-10-27 06:00:44 +0000

    satart

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,10 +1,17 @@
 #include "main.h"
 /**
- *_memset - fills the first n bytes of the memory area pointed to by s with the constant byte b
- * @n
- * @s
- * @b
- * retern: a pointer to the memory area s
- */
+*_memset - fills the first n bytes of the memory area pointed to by s with the constant byte b
+* @n: how many bytes to fill
+* @s: the pointer
+* @b: the actual bite number that we will use
+* retern: a pointer to the memory area s
+*/
 char *_memset(char *s, char b, unsigned int n);
-
+{
+unsigned int b;
+for (b = 0; b < n; b++)
+{
+s[i] = b;
+}
+return (s);
+}
