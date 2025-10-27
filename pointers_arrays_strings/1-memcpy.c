02c00e924be8199c3ce9c85f1dd4ccde11a91cc5
badriahalmalki <badryahbalmalki@gmail.com>
2025-10-27 06:48:41 +0000

    satart

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -3,7 +3,7 @@
 * _memcpy - copies memory area
 * @n: the byet the we want to copy
 * @srs: the memory area
-* @dest: whare we want the copy to go 
+* @dest: whare we want the copy to go
 * return: a pointer to dest
 */
 char *_memcpy(char *dest, char *src, unsigned int n)
