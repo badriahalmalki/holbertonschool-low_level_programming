913240bd43588c578aa8674b1a2b0a4b466f7b0d
badriahalmalki <badryahbalmalki@gmail.com>
2025-10-28 15:52:58 +0000

    1

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,23 @@
+#include "maine.h"
+#include <stdio.h>
+/**
+* _chessboard -  prints the chessboard
+* @a:
+* Retern: a chessboard
+*/
+void print_chessboard(char (*a)[8]);
+void printChessboard()
+{
+int  main() {
+int arr[8][8] = {r,k,b,q,k,b,k,r};
+int arr[8][8] = {' ',' '};
+int i, j;
+
+for (i=0; i<1; i++){
+for (j=0; j<5; j++){
+printf("%4d", arr[i][j]);
+}
+printf("\n");
+}
+return 0;
+}
