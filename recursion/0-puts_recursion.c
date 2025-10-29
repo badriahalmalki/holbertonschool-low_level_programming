281eff9fe93439466baa0932baf5a5b45db81607
badriahalmalki <badryahbalmalki@gmail.com>
2025-10-29 12:06:22 +0000

    1

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,7 +1,12 @@
 #include "main.h"
 #include <stdio.h>
-
+/**
+ * _puts_recursion - write a function that prints a string
+ * @s:
+ * Retern: string
+ */
 void _puts_recursion(char *s)
+
 {
 if (*s)
 {
@@ -10,6 +15,6 @@ _puts_recursion(s + 1);
 }
 else
 {
-putchar('\n');
+printf('\n');
 }
 }
