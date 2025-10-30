bf7b348ac5aecef24f836b3f3c50670b867acfbb
badriahalmalki <badryahbalmalki@gmail.com>
2025-10-30 17:32:55 +0300

    1

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,20 +1,13 @@
-#incloud "main.h"
+#include "main.h"
 
 int is_prime_number(int n)
-int n;
-
-do {
-    printf("Input a positive integer greater than 1: ");
-            scanf("%d", &n);
-
+{
     if (n <= 1)
-        printf("Invalid input. Try again.\n");
-} while (n <= 1);
-
-
-if (IsPrime(n))
-    printf("%d is a prime number.\n", n);
-else 
-    printf("%d is not a prime number.\n", n);
-
-return 0;
+        return 0;
+    for (int i = 2; i * i <= n; i++)
+    {
+        if (n % i == 0)
+            return 0;
+    }
+    return 1;
+}
