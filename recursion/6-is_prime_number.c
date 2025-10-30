9edbc285f4d28a3ad6fbf68bce6b8bc271fbf074
badriahalmalki <badryahbalmalki@gmail.com>
2025-10-30 17:48:33 +0300

    1

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -5,13 +5,14 @@
  * Return: 1 if n is prime, 0 otherwise
  */
 int is_prime_number(int n)
+int i;
 {
-    if (n <= 1)
-        return 0;
-    for (int i = 2; i * i <= n; i++)
-    {
-        if (n % i == 0)
-            return 0;
-    }
-    return 1;
+if (n <= 1)
+return 0;
+for (i = 2; i * i <= n; i++)
+{
+if (n % i == 0)
+return 0;
+}
+return 1;
 }
