2c66bf206c02f5deb13a1d95a74a2dcf49767a8d
badriahalmalki <badryahbalmalki@gmail.com>
2025-10-31 12:46:07 +0000

    1

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,14 @@
+#include <main.h>
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: the number to find the square root of
+ *
+ * Return: the natural square root, or -1 if not a perfect square
+ */
+int _sqrt_recursion(int n)
+{
+    if (n < 0)
+        return (-1);
+    return (find_root(n, 0));
+}
