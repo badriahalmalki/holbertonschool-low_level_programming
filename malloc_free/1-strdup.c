80cba6eaba9778ae02253ed705c4d1875503313f
badriahalmalki <badryahbalmalki@gmail.com>
2025-11-05 17:14:43 +0300

    1

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -17,8 +17,8 @@ char *_strdup(char *str)
 if (str == NULL)
 return (NULL);
 }
-for (len = 0; str[len] != '\0'; len++, str++)
-
+for (len = 0; str[len] != '\0'; len++)
+;
 dup = malloc(sizeof(char) * (len + 1));
 if (dup == NULL)
 return (NULL);
