c9c99b937a175e6a418263014c6a3554d44c74d4
badriahalmalki <badryahbalmalki@gmail.com>
2025-11-07 15:07:09 +0300

    1

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -10,19 +10,29 @@
 
 char *str_concat(char *string1, char *string2)
 {
+    char *concat_str;
+    unsigned int len1 = 0, len2 = 0, i, j;
 
+    if (string1 == NULL)
+        string1 = "";
+    if (string2 == NULL)
+        string2 = "";
 
-    if (string1 == NUll, && string2 == NULL)
-    return (NULL);
-        {
-                int length1 = 0;
-                int length2 = 0;
-            for (string1[length1] !='\0') && (string2[length2] !='\0')
-            length1++;
-            length2++;
+    while (string1[len1] != '\0')
+        len1++;
+    while (string2[len2] != '\0')
+        len2++;
 
-        }
+    concat_str = malloc(sizeof(char) * (len1 + len2 + 1));
+    if (concat_str == NULL)
+        return (NULL);
+
+    for (i = 0; i < len1; i++)
+        concat_str[i] = string1[i];
+    for (j = 0; j < len2; j++)
+        concat_str[i + j] = string2[j];
+
+    concat_str[i + j] = '\0';
+
+    return (concat_str);
 }
-
-
-    
