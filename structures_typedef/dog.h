7e950a3d14752433c00a04184b2791e8d10117af
badriahalmalki <badryahbalmalki@gmail.com>
2025-11-08 21:12:03 +0300

    2

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -16,9 +16,12 @@ struct dog
 	char *owner;
 
 };
+/* Define typedef BEFORE using dog_t */
+typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+
 #endif
