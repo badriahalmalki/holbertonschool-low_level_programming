8d4224832ff650c3ab1617623b3cb8d402e74c93
badriahalmalki <badryahbalmalki@gmail.com>
2025-11-08 19:34:40 +0300

    3

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdlib.h>
+
+
+int *array_range(int min, int max)
+{
+	int *num_array;      /* pointer to the new array in memory */
+
+	int count = 0;       /* count: index while filling the array */
+
+	int total_numbers;  /* total numbers between min and max */
+
+	if (min >= max)
+		return (NULL);
+
+	total_numbers = max - min + 1;
+
+	num_array = malloc(sizeof(int) * total_numbers);
+	if (num_array == NULL)
+		return (NULL);
+
+	for (count = 0; count < total_numbers; count++)
+		num_array[count] = min + count; /* start at min and increase */
+
+	return (num_array);
+}
