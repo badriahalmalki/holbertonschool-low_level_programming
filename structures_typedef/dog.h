6440187af77f7e2286c48e48964e13a8a8f40633
badriahalmalki <badryahbalmalki@gmail.com>
2025-11-08 20:30:13 +0300

    dogggy<3

---
+++ structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+* struct dog - Structure for dog information
+* @name: Name of the dog
+* @age: Age of the dog
+* @owner: Owner of the dog
+*/
+struct dog
+{
+	char *name;
+
+	float age;
+
+	char *owner;
+
+};
+
+#endif
