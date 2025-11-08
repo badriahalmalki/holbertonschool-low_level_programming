5fee3474f7e654cbc947fee02aa2b95a110b7235
badriahalmalki <badryahbalmalki@gmail.com>
2025-11-08 19:23:45 +0300

    2

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -1,37 +1,21 @@
 #include "main.h"
 #include <stdlib.h>
 
-/**
-* _calloc - allocates memory for an array and sets it to zero
-* @array_no: number of elements to allocate
-* @array_size: size (in bytes) of each element
-*
-* Return: pointer to the allocated memory,
-*         or NULL if malloc fails or input is invalid
-*/
-void *_calloc(unsigned int array_no, unsigned int array_size)
+void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	char *memory_block; /* pointer to the allocated memory */
+    char *ptr;
+    unsigned int total_size, i;
 
-	unsigned int idx = 0; /* idx: index used to set each byte to zero */
+    if (nmemb == 0 || size == 0)
+        return (NULL);
 
-	unsigned int total_size; /* total_size: total bytes to allocate */
+    total_size = nmemb * size;
+    ptr = malloc(total_size);
+    if (ptr == NULL)
+        return (NULL);
 
-	/* If either number or size is zero, allocation makes no sense */
-	if (array_no == 0 || array_size == 0)
-		return (NULL);
+    for (i = 0; i < total_size; i++)
+        ptr[i] = 0;
 
-	/* Calculate the memory needed */
-	total_size = array_no * array_size;
-
-	/* Allocate memory */
-	memory_block = malloc(array_size);
-	if (memory_block == NULL)
-		return (NULL);
-
-	/* Fill memory with zeros */
-	for (idx = 0; idx < total_size; idx++)
-		memory_block[idx] = 0;
-
-	return (memory_block);
+    return (ptr);
 }
