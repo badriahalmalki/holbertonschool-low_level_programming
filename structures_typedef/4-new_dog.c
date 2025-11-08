b5f0f67b4eca1bb2780022457ba4c87795594788
badriahalmalki <badryahbalmalki@gmail.com>
2025-11-08 21:26:31 +0300

    fix

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,44 +1,21 @@
 #include "dog.h"
 #include <stdlib.h>
-#include <string.h>
 
 /**
- * new_dog - creates a new dog
- * @name: dog's name
- * @age: dog's age
- * @owner: dog's owner
- * Return: pointer to new dog_t, or NULL if fail
- */
+* new_dog - creates a new dog
+* @name: dog's name
+* @age: dog's age
+* @owner: dog's owner
+* Return: pointer to new dog_t, or NULL if fail
+*/
 dog_t *new_dog(char *name, float age, char *owner)
+
+int _strlen(char *s)
 {
-    dog_t *newdog;
-    char *name_copy, *owner_copy;
+	int len = 0;
 
-    newdog = malloc(sizeof(dog_t)); /* Allocate memory for new dog */
-    if (newdog == NULL)
-        return (NULL);
+	while (s[len] != '\0')
+		len++;
 
-    name_copy = malloc(strlen(name) + 1); /* Allocate memory for name */
-    if (name_copy == NULL)
-    {
-        free(newdog);
-        return (NULL);
-    }
-
-    owner_copy = malloc(strlen(owner) + 1); /* Allocate memory for owner */
-    if (owner_copy == NULL)
-    {
-        free(name_copy);
-        free(newdog);
-        return (NULL);
-    }
-
-    strcpy(name_copy, name);
-    strcpy(owner_copy, owner);
-
-    newdog->name = name_copy; /* Assign copied name */
-    newdog->age = age;
-    newdog->owner = owner_copy;
-
-    return (newdog);
+	return (len);
 }
