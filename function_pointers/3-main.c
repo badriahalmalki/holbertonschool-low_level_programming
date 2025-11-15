04cadcaa7fecd009236a5ad13ce14507cab70141
badriahalmalki <badryahbalmalki@gmail.com>
2025-11-15 15:47:57 +0300

    2

---
+++ function_pointers/3-main.c
@@ -0,0 +1,34 @@
+#include "3-calc.h"
+
+/**
+ * main - performs simple operations
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: always 0
+ */
+int main(int argc, char *argv[])
+{
+	int a, b;
+	int (*op_func)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	op_func = get_op_func(argv[2]);
+	if (op_func == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+
+	printf("%d\n", op_func(a, b));
+
+	return (0);
+}
