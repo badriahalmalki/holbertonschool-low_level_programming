d9c947191ffd7fc42cc4e32be41614dde71736d7
badriahalmalki <badryahbalmalki@gmail.com>
2025-12-02 16:00:06 +0300

    make sure it comp and betty

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -2,41 +2,43 @@
 #include <stdlib.h> /* For malloc and free */
 #include <string.h> /* For strdup */
 
+/**
+ * add_dnodeint_end - Adds a new node at the end of a doubly linked list
+ * @head: Pointer to the head of the list
+ * @n: Integer value to be added to the new node
+ *
+ * Return: Address of the new node, or NULL if it failed
+ */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
 	dlistint_t *new_node;
 	dlistint_t *temp;
-	unsigned int len = 0;
 
-	/* Create a new node */
-	new_node = malloc(sizeof(dlistint_t));
+	/* Allocate memory for the new node */
+	new_node = (dlistint_t *)malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 		return (NULL);
 
-	/* Duplicate the string and calculate its length */
-	new_node->const int n = strdup(const int n));
-	if (new_node->const int n == NULL)
-	{
-		free(new_node);
-		return (NULL);
-	}
-	while (const int n[len])
-		len++;
-	new_node->len = len;
+	/* Initialize the new node */
+	new_node->n = n;
 	new_node->next = NULL;
 
 	/* If the list is empty, make the new node the head */
 	if (*head == NULL)
 	{
+		new_node->prev = NULL;
 		*head = new_node;
 		return (new_node);
 	}
 
-	/* Otherwise, traverse to the end of the list and add the new node */
+	/* Traverse to the end of the list */
 	temp = *head;
 	while (temp->next != NULL)
 		temp = temp->next;
+
+	/* Link the new node at the end */
 	temp->next = new_node;
+	new_node->prev = temp;
 
 	return (new_node);
 }
