46bc13cc9f9b1951ca69400ceb3dd8733f9a8326
badriahalmalki <badryahbalmalki@gmail.com>
2025-12-20 19:36:40 +0300

    11

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -1,5 +1,11 @@
 #include "hash_tables.h"
 
+/**
+* key_index - Gives you the index of a key
+* @key: The key
+* @size: The size of the array of the hash table
+* Return: The index at which the key/value pair should be stored in the array
+*/
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	unsigned long int hash;
--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -1,20 +1,26 @@
 #include "hash_tables.h"
 
+/**
+* hash_table_get - Retrieves a value associated with a key
+* @ht: The hash table to look into
+* @key: The key to search for
+* Return: The value associated with the key, or NULL
+*/
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-    hash_node_t *current;
-    unsigned long int index;
+	hash_node_t *current;
+	unsigned long int index;
 
-    if (ht == NULL || key == NULL || *key == '\0')
-        return (NULL);
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (NULL);
 
-    index = key_index((const unsigned char *)key, ht->size);
-    current = ht->array[index];
+	index = key_index((const unsigned char *)key, ht->size);
+	current = ht->array[index];
 
-    while (current != NULL)
-        if (strcmp(current->key, key) == 0)
-            return (current->value);
-        current = current->next;
+	while (current != NULL)
+		if (strcmp(current->key, key) == 0)
+			return (current->value);
+		current = current->next;
 
-    return (NULL);
+	return (NULL);
 }
