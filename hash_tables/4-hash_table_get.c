22715d4c747c8bd12b9cf922ce0954b2e2b01f2c
badriahalmalki <badryahbalmalki@gmail.com>
2025-12-20 19:57:09 +0300

    11

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -18,9 +18,10 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	current = ht->array[index];
 
 	while (current != NULL)
+	{
 		if (strcmp(current->key, key) == 0)
 			return (current->value);
 		current = current->next;
-
+	}
 	return (NULL);
 }
