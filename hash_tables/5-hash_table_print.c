8a1e42f920903986d5f044eaccaa86e58f403cb3
badriahalmalki <badryahbalmalki@gmail.com>
2025-12-20 20:00:57 +0300

    11

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -1,26 +1,32 @@
 #include "hash_tables.h"
 
+/**
+* hash_table_print - Prints a hash table.
+* @ht: The hash table to print.
+*/
 void hash_table_print(const hash_table_t *ht)
 {
-    unsigned long int i;
-    int first = 1;
-    hash_node_t *current;
+	unsigned long int i;
 
-    if (ht == NULL)
-        return;
+	int first = 1;
 
-    printf("{");
-    for (i = 0; i < ht->size; i++)
-    {
-        current = ht->array[i];
-        while (current != NULL)
-        {
-            if (!first)
-                printf(", ");
-            printf("'%s': '%s'", current->key, current->value);
-            first = 0;
-            current = current->next;
-        }
-    }
-    printf("}\n");
+	hash_node_t *current;
+
+	if (ht == NULL)
+		return;
+
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		current = ht->array[i];
+		while (current != NULL)
+		{
+			if (!first)
+				printf(", ");
+			printf("'%s': '%s'", current->key, current->value);
+			first = 0;
+			current = current->next;
+		}
+	}
+	printf("}\n");
 }
