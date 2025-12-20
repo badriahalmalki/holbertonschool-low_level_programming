db9d2ccc1d815a8fc9a6cc7406d0e24b2bd9b157
badriahalmalki <badryahbalmalki@gmail.com>
2025-12-20 20:05:51 +0300

    11

--- hash_tables/6-hash_table_delete.c
+++ hash_tables/6-hash_table_delete.c
@@ -15,12 +15,14 @@ void hash_table_delete(hash_table_t *ht)
 	{
 		current = ht->array[i];
 		while (current != NULL)
+		{
 			temp = current;
 			current = current->next;
 			free(temp->key);
 			free(temp->value);
 			free(temp);
 	}
+} /* end for loop */
 	free(ht->array);
 	free(ht);
 }
